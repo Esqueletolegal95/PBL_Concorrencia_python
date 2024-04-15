d2a32224ac690bf9455402129c513a80fd832c91
Pedro Santos <113029820+Esqueletolegal95@users.noreply.github.com>
2024-04-15 12:07:35 -0300

    Comunicação HTTP

--- dispositivo/Dispositivo.py
+++ dispositivo/Dispositivo.py
@@ -6,6 +6,9 @@ import threading
 
 temperatura = random.randint(0, 34)
 
+def modificarTemperatura(novaTemperatura):
+    return novaTemperatura
+
 def get_host_ip_address():
     """
     Função para obter o endereço IP do host.
@@ -27,7 +30,7 @@ def enviar_mensagem_udp(temperatura):
                 
                 s.sendto(mensagem_json.encode(), ("localhost", 1234))  # Enviar via UDP
                 
-                print(f"Mensagem UDP enviada: {mensagem_json}")
+
                 
                 time.sleep(1)
     except Exception as e:
@@ -36,10 +39,10 @@ def enviar_mensagem_udp(temperatura):
 def receber_mensagem_tcp():
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((host_ip, 1235))
+            s.bind(("localhost", 5000))
             s.listen(1)  # Aceitar apenas uma conexão por vez
 
-            print(f"Aguardando conexão TCP em {host_ip}:1235...")
+            print(f"Aguardando conexão TCP em {host_ip}:5000...")
             conn, addr = s.accept()  # Aceitar conexão
             print(f"Conexão estabelecida com {addr}")
 
